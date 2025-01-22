3fd51a039f15db4e899141e044e2c831c1f8af4c
lesguillemets <kgkacko+github@gmail.com>
2025-01-22 16:01:24 +0900

    specify setting separately

--- src/video/save.rs
+++ src/video/save.rs
@@ -13,14 +13,23 @@ pub fn save_mat_to(filename: &str, img: &Mat) -> () {
 /// * 一連のフレームを`{base_name}_fr{serial}.{ext}` で保存
 /// * `&[(usize, Mat)]`: 保存したい [Mat] と，それに振るべき番号の列
 /// * dir: 保存先のディレクトリ
-pub fn save_mats_as(base_name: &str, dir: &str, ext: &str, imgs: &[(usize, Mat)]) {
+pub fn save_mats_as(stg: &SpawnSettings, imgs: &[(usize, Mat)]) {
     // TODO: 桁数
-    let dir = PathBuf::from(dir);
+    let dir = PathBuf::from(&stg.dir);
     if !dir.is_dir() {
         panic!("video::base::save_mats_as: dir not directory");
     }
+    let base = &stg.base_name;
+    let ext = &stg.ext;
     for (serial, im) in imgs.iter() {
-        let file = dir.join(format!("{base_name}_fr{serial:04}.{ext}"));
+        let file = dir.join(format!("{base}_fr{serial:04}.{ext}"));
         save_mat_to(file.to_str().unwrap(), im);
     }
+}
+
+/// 連番でできるたくさんのファイルを保存するような場合の設定
+pub struct SpawnSettings {
+    base_name: String,
+    dir: String,
+    ext: String,
 }
